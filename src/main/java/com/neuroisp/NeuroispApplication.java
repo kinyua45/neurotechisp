267ed1cf6d46e0477d8b04adb5e66a15e74dda9c
wythathello <patrickwainaina7322@gmail.com>
2026-02-08 21:37:19 +0300

    first commit

---
+++ src/main/java/com/neuroisp/NeuroispApplication.java
@@ -0,0 +1,13 @@
+package com.neuroisp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroispApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuroispApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neuroisp/NeuroispApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuroisp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroispApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
